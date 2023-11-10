d28985d6b451d8d0cbd3ebeac5335e6aa28cc235
Corentin <99296190+Cory83500@users.noreply.github.com>
2023-11-10 16:20:18 +1100

    ajout de la fonction anglais et correction des lien home

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var link = document.querySelector("a[href='#faq-area']");
     
-    link.addEventListener("click", function(event) {
-        event.preventDefault();
-        
-        // Récupérez la position de la section FAQ
-        var faqSection = document.getElementById("faq-area");
-        var faqSectionPosition = faqSection.offsetTop;
-        
-        // Faites défiler la page jusqu'à la section FAQ
-        window.scrollTo({
-            top: faqSectionPosition,
-            behavior: "smooth" // Cette option permet un défilement en douceur
+    if (link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            
+            // Récupérez la position de la section FAQ
+            var faqSection = document.getElementById("faq-area");
+            var faqSectionPosition = faqSection.offsetTop;
+            
+            // Faites défiler la page jusqu'à la section FAQ
+            window.scrollTo({
+                top: faqSectionPosition,
+                behavior: "smooth" // Cette option permet un défilement en douceur
+            });
         });
-    });
+    }
 });
 
 
